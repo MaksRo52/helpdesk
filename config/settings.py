1d30b62ddac1a8c81b402f54c9867e4d5af0ec47
m.romantsov <romancov_dzr@mail.ru>
2024-12-13 10:36:32 +0300

    add celery

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,7 @@ CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL") # Например, Redis, к
 # URL-адрес брокера результатов, также Redis
 CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 # Часовой пояс для работы Celery
-CELERY_TIMEZONE = "Europe/Moscow"
+CELERY_TIMEZONE = TIME_ZONE
 # Флаг отслеживания выполнения задач
 CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
